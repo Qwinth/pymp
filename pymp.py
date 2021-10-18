1fafe023a48d9ce97dd423c4f7ea539ba1f1b60f
Qwinth <76917271+Qwinth@users.noreply.github.com>
2021-10-18 10:34:46 +0300

    Add files via upload

---
+++ pymp.py
@@ -0,0 +1,37 @@
+import time, sys
+from pygame import mixer
+from multiprocessing import Process, Pipe
+import time
+mixer.init()
+def process(cc):
+    mixer.music.load(cc.recv()[0])
+    recv = cc.recv()
+    if recv == ['play']:
+        mixer.music.play()
+        while True:
+            if mixer.music.get_busy() == 0:
+                break
+    
+        
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    if sys.argv[1] == '-p' or sys.argv[1] == '--parallel':
+        pc, cc = Pipe()
+        Process(target=process, args=(cc,)).start()
+        mixer.music.load(sys.argv[2])
+        pc.send([sys.argv[3]])
+        pc.send(['play'])
+        time.sleep(0.1)
+        mixer.music.play()
+        while True:
+            if mixer.music.get_busy() == 0:
+                break
+    else:
+        mixer.music.load(sys.argv[1])
+        mixer.music.play()
+        while True:
+            if mixer.music.get_busy() == 0:
+                break
+
